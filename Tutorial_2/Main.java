62202eae1aa4313c31c7de1fd3909a91e4613c9d
faizaannoorariya <faizaannoorariya@gmail.com>
2026-06-09 04:30:16 +0000

    Ariya's success

---
+++ Tutorial_2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s2 = new Student("Faiza", 21, 4.00);
+        s2.displayInfo();
+        s2.study();
+        s2.takeExam();
+    } 
+}
